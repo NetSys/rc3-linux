17ae41c2273c8caa7e24f28a6548bd587e686dea
Radhika Mittal <radhika@c32.Millennium.Berkeley.EDU>
2015-05-09 22:39:08 -0700

    RC3 linux code

---
+++ rc3-kernel-3.x/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 88 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#88 SMP Sat Sep 21 22:09:06 PDT 2013"
+#define LINUX_COMPILE_BY "radhika"
+#define LINUX_COMPILE_HOST "c12"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
---
+++ rc3-kernel-3.x/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.2.48+"
